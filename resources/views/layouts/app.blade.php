2fcff7f28f70bf168db589296358a5b3dc3e44da
HelfanzaNanda <elvannnd@gmail.com>
2021-04-13 17:01:37 +0700

    animation menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,23 +16,23 @@
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     @stack('scripts')
     <script>
       let menuActive = false;
-      $(".toogle-menu").on('click', function() {
+      $(".toogle-menu").click(function() {
         if (!menuActive) {
             menuActive = true
-            $('.header').hide()
-            $('#menu').show();
+            $('.header').hide("slow")
+            $('#menu').show("slow")
             $('.toogle-menu.open').addClass('d-none')
             $('.toogle-menu.close').removeClass('d-none')
         }else{
             menuActive = false
-            $('.header').show()
-            $('#menu').hide();
+            $('.header').show("slow")
+            $('#menu').hide("slow");
             $('.toogle-menu.open').removeClass('d-none')
             $('.toogle-menu.close').addClass('d-none')
         }
--- resources/views/our_proccess/index.blade.php
+++ resources/views/our_proccess/index.blade.php
@@ -1,18 +1,19 @@
 @extends('layouts.app')
 @section('content')
-    <div class="header-landing-page bg-yellow">
+    <div class="parent-menu">
         <x-menu/>
+    </div>
+    <div class="container-main navbar-our-process padding-top-32">
+        
+        <div class="row justify-content-between">
+            <div class="d-inline"><img src="{{ asset('images/logo.png') }}" alt="felicity" width="80" height="60"></div>
+            <img class="toogle-menu open" style="cursor: pointer; z-index: 2;" src="{{ asset('images/icons/menu.png') }}" width="44" height="44">
+            <img class="toogle-menu close d-none" style="cursor: pointer; z-index: 2;" src="{{ asset('images/icons/close.png') }}" width="44" height="44">
+        </div>
+    </div>
+    <div class="header-landing-page bg-yellow">
         <img class="wafe our-proccess" src="{{ asset('images/our-proccess/bg-header-yellow-full.png') }}" alt="">
         <img class="circle-our-proccess" src="{{ asset('images/our-proccess/circle.png') }}" alt="">
-        <div class="container-main navbar-our-process padding-top-32">
-            <div class="row justify-content-between">
-                <div class="d-inline"><img src="{{ asset('images/logo.png') }}" alt="felicity" width="80" height="60"></div>
-                <img class="toogle-menu open" style="cursor: pointer; z-index: 2;" src="{{ asset('images/icons/menu.png') }}" width="44" height="44">
-                <img class="toogle-menu close d-none" style="cursor: pointer; z-index: 2;" src="{{ asset('images/icons/close.png') }}" width="44" height="44">
-            </div>
-        </div>
-       
-        
         <div class="header padding-top-288" style="transition: all 0.3s ease;">
             <div class="container-main margin-bottom-178">
                 <div class="font-weight-700 font-size-50 font-kollektif">Research</div>
